2269defee80b6c08dad48866049e3156f8fc90b4
0x0alikacimi <alikacimi277@gmail.com>
2025-07-26 18:36:13 +0100

    start

---
+++ ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac , char **av)
+{
+	if (ac == 3)
+	{
+		
+		return (0);
+	}
+	else
+	{
+		std::cerr << "invalid input" << std::endl;
+		return (1);
+	}
+
+}
